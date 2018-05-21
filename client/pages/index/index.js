1746381b0ccf76b933d775747e01b96ccf4b742f
YogurtLJH <316880624@qq.com>
2018-05-21 15:21:07 +0800

    updata

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -2,7 +2,8 @@ var app = getApp()
 Page({
   data: {
     navbar: ['失物招领', '寻物启事'],
-    currentTab: 0
+    currentTab: 0,
+    hiddenmodalput: true,//搜索框初始为不可见
   },
   navbarTap: function (e) {
     this.setData({
@@ -29,6 +30,25 @@ Page({
     wx.navigateTo({
       url: '../user/user',
     })
-  }
+  },
+
+  //搜索框
+  modalinput: function () {
+    this.setData({
+      hiddenmodalput: !this.data.hiddenmodalput
+    })
+  },
+  //取消按钮  
+  cancel: function () {
+    this.setData({
+      hiddenmodalput: true
+    });
+  },
+  //确认  
+  confirm: function () {
+    this.setData({
+      hiddenmodalput: true
+    })
+  }  
 
 })
