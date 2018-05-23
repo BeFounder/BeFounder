e8927587b92872d5b7dfc5887dcf3ac1df6858a5
Ruisfutrue <2273709996@qq.com>
2018-05-24 00:26:41 +0800

    点击button实现获取userinfo
    点击button获取昵称，头像等信息。

--- client/pages/title/title.js
+++ client/pages/title/title.js
@@ -1,11 +1,18 @@
 // pages/title/title.js
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var util = require('../../utils/util.js')
+var config = require('../../config')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: '',
   },
 
   /**
@@ -97,4 +104,81 @@ Page({
     })
 >>>>>>> 543f5f4d4cb1f67a1c3e3cd33e6f702988a55c0c
   }*/
+
+  bindGetUserInfo: function (e) {
+    if (this.data.logged) return;
+
+    util.showBusy('正在登录');
+
+    var that = this;
+    var userInfo = e.detail.userInfo;
+
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+
+          // 检查登录是否过期
+          wx.checkSession({
+            success: function () {
+              // 登录态未过期
+              util.showSuccess('登录成功');
+              that.setData({
+                userInfo: userInfo,
+                logged: true
+              })
+              console.log(userInfo);
+            },
+
+            fail: function () {
+              qcloud.clearSession();
+              // 登录态已过期，需重新登录
+              var options = {
+                encryptedData: e.detail.encryptedData,
+                iv: e.detail.iv,
+                userInfo: userInfo
+              }
+              that.doLogin(options);
+            },
+          });
+        } else {
+          util.showModel('用户未授权', e.detail.errMsg);
+        }
+      }
+    });
+  },
+
+  doLogin: function (options) {
+    var that = this;
+
+    wx.login({
+      success: function (loginResult) {
+        var loginParams = {
+          code: loginResult.code,
+          encryptedData: options.encryptedData,
+          iv: options.iv,
+        }
+        qcloud.requestLogin({
+          loginParams, success() {
+            util.showSuccess('登录成功');
+
+            that.setData({
+              userInfo: options.userInfo,
+              logged: true
+            })
+          },
+          fail(error) {
+            util.showModel('登录失败', error)
+            console.log('登录失败', error)
+          }
+        });
+      },
+      fail: function (loginError) {
+        util.showModel('登录失败', loginError)
+        console.log('登录失败', loginError)
+      },
+    });
+  }
+
+
 })
