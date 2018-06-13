b7ab9c839adbc56a98a41ec5089ef88a2a302ee6
YogurtLJH <316880624@qq.com>
2018-06-13 22:59:28 +0800

    yg-6.13.22:59

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -67,93 +67,9 @@ Page({
       timingFunction: 'ease-out'
     })
     animationPlus.rotateZ(180).step();
-    animationcollect.translate(-100, -100).rotateZ(180).opacity(1).step();
-    animationTranspond.translate(-140, 0).rotateZ(180).opacity(1).step();
-    animationInput.translate(-100, 100).rotateZ(180).opacity(1).step();
-    this.setData({
-      animPlus: animationPlus.export(),
-      animCollect: animationcollect.export(),
-      animTranspond: animationTranspond.export(),
-      animInput: animationInput.export(),
-    })
-  },
-  //收回动画  
-  takeback: function () {
-    //plus逆时针旋转  
-    var animationPlus = wx.createAnimation({
-      duration: 500,
-      timingFunction: 'ease-out'
-    })
-    var animationcollect = wx.createAnimation({
-      duration: 500,
-      timingFunction: 'ease-out'
-    })
-    var animationTranspond = wx.createAnimation({
-      duration: 500,
-      timingFunction: 'ease-out'
-    })
-    var animationInput = wx.createAnimation({
-      duration: 500,
-      timingFunction: 'ease-out'
-    })
-    animationPlus.rotateZ(0).step();
-    animationcollect.translate(0, 0).rotateZ(0).opacity(0).step();
-    animationTranspond.translate(0, 0).rotateZ(0).opacity(0).step();
-    animationInput.translate(0, 0).rotateZ(0).opacity(0).step();
-    this.setData({
-      animPlus: animationPlus.export(),
-      animCollect: animationcollect.export(),
-      animTranspond: animationTranspond.export(),
-      animInput: animationInput.export(),
-    })
-  }, plus: function () {
-    if (this.data.isPopping) {
-      //缩回动画  
-      this.popp();
-      this.setData({
-        isPopping: false
-      })
-    } else if (!this.data.isPopping) {
-      //弹出动画  
-      this.takeback();
-      this.setData({
-        isPopping: true
-      })
-    }
-  },
-  input: function () {
-    console.log("input")
-  },
-  transpond: function () {
-    console.log("transpond")
-  },
-  collect: function () {
-    console.log("collect")
-  },
-
-  //弹出动画  
-  popp: function () {
-    //plus顺时针旋转  
-    var animationPlus = wx.createAnimation({
-      duration: 500,
-      timingFunction: 'ease-out'
-    })
-    var animationcollect = wx.createAnimation({
-      duration: 500,
-      timingFunction: 'ease-out'
-    })
-    var animationTranspond = wx.createAnimation({
-      duration: 500,
-      timingFunction: 'ease-out'
-    })
-    var animationInput = wx.createAnimation({
-      duration: 500,
-      timingFunction: 'ease-out'
-    })
-    animationPlus.rotateZ(180).step();
-    animationcollect.translate(-100, -100).rotateZ(180).opacity(1).step();
-    animationTranspond.translate(-140, 0).rotateZ(180).opacity(1).step();
-    animationInput.translate(-100, 100).rotateZ(180).opacity(1).step();
+    animationcollect.translate(10, -60).rotateZ(180).opacity(1).step();
+    animationTranspond.translate(-50, -50).rotateZ(180).opacity(1).step();
+    animationInput.translate(-60, 10).rotateZ(180).opacity(1).step();
     this.setData({
       animPlus: animationPlus.export(),
       animCollect: animationcollect.export(),
@@ -452,6 +368,16 @@ Page({
 
 
   },
+  getMap: function (e) {
+    var i = e.currentTarget.dataset.noid
+    var that = this
+
+    wx.openLocation({
+      latitude: parseFloat(that.data.titleArray[i]["Latitude"]),
+      longitude: parseFloat(that.data.titleArray[i]["Longitude"]),
+    })
+  }
+
   
 
 })
